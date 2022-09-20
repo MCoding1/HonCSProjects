961959751397f22e8b61e6d321c1e6e2ac44887e
Mel <melodieebrahimi24@marlborough.org>
2022-09-19 22:20:56 -0700

    fs

--- tamaplegatchi/game.js
+++ tamaplegatchi/game.js
@@ -1,16 +1,26 @@
-function setup()
-{
+//function setup()
+//{
   //let fs = fullscreen();
   //fullscreen(!fs);
-background(0);
+//background(0);
   //createCanvas(800, 800);
 
 
 
+//}
+
+//function draw()
+//{
+//createCanvas(fullscreen());
+
+//}
+
+function setup() {
+  background(200);
 }
-
-function draw()
-{
-createCanvas(fullscreen());
-
+function mousePressed() {
+  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
 }
