ca6546fdb2a953a390f84b69e0ccf9b3770494ce
Mel <melodieebrahimi24@marlborough.org>
2022-12-15 11:42:13 -0800

    cat

--- tamaplegotchi/game.js
+++ tamaplegotchi/game.js
@@ -16,7 +16,8 @@
 //}
 
 function setup() {
-  background(200);
+  createCanvas(500, 500);
+  background(255);
 }
 function mousePressed() {
   if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
@@ -24,3 +25,28 @@ function mousePressed() {
     fullscreen(!fs);
   }
 }
+
+let img;
+let img2;
+let img3;
+function preload() {
+  img = loadImage('data/shell.png');
+  img2 = loadImage('data/cat.png');
+  img3 = loadImage('data/orangecat.png');
+}
+function draw() {
+  // Top-left corner of the img is at (0, 0)
+  // Width and height are the img's original width and height
+  image(img, 0, 0, 500, 500);
+  //image(img2, 160, 200, 180, 180);
+  image(img3, 200, 250, 100, 100);
+}
+
+
+//How to put temporary web server
+//python3 -m http.server
+//how to open game
+//http://localhost:8000/tamaplegotchi.html
+//how to close the server
+//cntrl c <-NOT COMMAND C
+//
