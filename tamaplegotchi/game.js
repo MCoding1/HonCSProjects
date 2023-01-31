ab7e15cb07fcc0ea3ca82b8c7b1f8e8994980c89
Mel <melodieebrahimi24@marlborough.org>
2023-01-31 09:13:57 -0800

    popup made

--- tamaplegotchi/game.js
+++ tamaplegotchi/game.js
@@ -17,6 +17,8 @@
 
 //!!!!!!UOI@IU#$UOI#O# use assignment 9 from processing semester 1 for game states
 
+let on = false;
+
 function setup() {
   createCanvas(500, 500);
   background(250, 203, 152);
@@ -29,6 +31,11 @@ function mousePressed() {
     let fs = fullscreen();
     fullscreen(!fs);
   }
+//pop-up
+  //light up the rectangle when clicking inside of it
+  if (mouseX > (width*3 / (4) - 2) && mouseX < (width*3 / (4) - 2)+width/6 && mouseY > (height / 3 - 2) / 3 && mouseY < (height / 3 - 2) +height/6) {
+    on = !on
+  }
 }
 
 let img;
@@ -40,18 +47,76 @@ function preload() {
   img2 = loadImage('data/cat.png');
   img3 = loadImage('data/orangecat.png');
   img4 = loadImage('data/catsleep.png');
+  img5 = loadImage('data/realcat.png');
+  img6 = loadImage('data/home.png');
 }
 function draw() {
+
+  noStroke();
+  fill(255, 181, 218);
+  rect(0,0,48,20);
+  fill(255);
+  textSize(8);
+  text('Fullscreen', 5, 12);
+
+
   // Top-left corner of the img is at (0, 0)
   // Width and height are the img's original width and height
   image(img, 0, 0, 500, 500);
   //image(img2, 160, 200, 180, 180);
-  image(img4, 200, 250, 200, 200);
+  image(img6, 172, 205, 150, 147); //home
+
+  image(img5, 200, 287, 100, 70); // cat
 
 //  ellipse(x,y,50,50);
 //  x++;
 //  y--:
 //  if(dist())
+
+
+
+//////***/////////***/////////***/////////***/////////***/////////***/////////***///
+
+//pop-up
+  let square = {
+    x: random(width*3 / (4) - 2, width*3 / (4) + 2),
+    y: random(height / 3 - 2, height / 3 + 2),
+    w: width / 6,
+    h: height / 6,
+  }
+
+
+  //draw a rectangle
+  noStroke();
+  fill(255, 255, 255);
+  rect(square.x, square.y, square.w, square.h);
+
+
+  fill(0);
+  textSize(10);
+  text('A Tamagotchi?', 383, 215);
+
+  //let the rectangle pop when mouse is inside of it
+  if (mouseX > (width*3 / (4) - 2) && mouseX < (width*3 / (4) - 2)+width/6 && mouseY > (height / 3 - 2) / 3 && mouseY < (height / 3 - 2) +height/6) {
+    square.x = width / 3;
+    square.y = height / 3;square.w = square.w * 2
+    square.h = square.h * 2
+  }
+
+  //define "on"
+  if (on) {
+    noStroke();
+    fill(255, 236, 184);
+    rect(width / 3, height / 3, width / 3, height / 3);
+
+    fill(0);
+    textSize(10);
+    text('This is a Tamagotchi...', 191, 215);
+  }
+  //////***/////////***/////////***/////////***/////////***/////////***/////////***///
+
+
+
 }
 
 // initially hide the divs using css or js
@@ -66,4 +131,4 @@ function draw() {
 //http://localhost:8000/tamaplegotchi.html
 //how to close the server
 //cntrl c <-NOT COMMAND C
-//
+//to update it do shift + reload
