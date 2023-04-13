cb402e1c713261ab3af47f19cfc1ad0feb07f800
Mel <melodieebrahimi24@marlborough.org>
2023-04-13 10:45:17 -0700

    game screens work now

--- tamaplegotchi/game.js
+++ tamaplegotchi/game.js
@@ -1,3 +1,9 @@
+//have camera and popup drawing first in an outside function, then go into game states for the screen
+//click screen to start game, click screen to restart game, left button goes to settings and moves through options, middle button selects option, right button goes back
+// or we can do left button settings, arrow keys to go through options, middle button to select and right button to go home?
+//get game screen for home, shop, and "action room" running, be able to name pet once on start screen, score changes when feed pet, if feed pet too much dies, end screen, get under the hood working
+//or can have start screen and end, left button goes to left side shop, right button goes to right side action room, middle button goes home, buy things by clicking on then and will get popup yes or no clik Y yes or N no
+
 //function setup()
 //{
   //let fs = fullscreen();
@@ -30,15 +36,46 @@
 
 let on = false;
 
+let action = false;
+
+let home = true;
+
+let shop = false;
+
 var startTime, endTime;
 
 const photo = new Boolean(false);
+
 
 function setup() {
 cnv = createCanvas(500, 500);
   background(250, 203, 152);
 //  x = 50;
 //  y = 50;
+}
+
+let img;
+let img2;
+let img3;
+let img4;
+let img5;
+let img6;
+let img7;
+let img8;
+function preload() {
+  img = loadImage('data/shell.png');
+  img2 = loadImage('data/cat.png');
+  img3 = loadImage('data/orangecat.png');
+  img4 = loadImage('data/catsleep.png');
+  img5 = loadImage('data/realcat.png');
+  img6 = loadImage('data/home.png');
+  img7 = loadImage('data/camera.png');
+  img8 = loadImage('data/flash.png');
+  img9 = loadImage('data/house.png');
+  img10 = loadImage('data/left.png');
+  img11 = loadImage('data/right.png');
+  img12 = loadImage('data/shop.png');
+  img13 = loadImage('data/action.png');
 }
 
 function mousePressed() {
@@ -61,26 +98,33 @@ function mousePressed() {
 //  startTime = new Date();
   //};
   }
+
+  //action screen
+  if (mouseX > 192 && mouseX < 207 && mouseY > 398 && mouseY < 413) {
+    action = true;
+    home = false;
+    shop = false;
+    //image(img13, 172, 205, 150, 147); //action
+
+    //image(img5, 230, 287, 80, 55); // cat
+    //image(img5, 230, 287, 200, 55); // mega cat
+
+  }
+
+  //home screen
+  if (mouseX > 237 && mouseX < 252 && mouseY > 410 && mouseY < 425) {
+    home = true;
+    action = false;
+    shop = false;
+  }
+  //shop screen
+  if (mouseX > 285 && mouseX < 300 && mouseY > 400 && mouseY < 415) {
+    shop = true;
+    home = false;
+    action = false;
+  }
 }
 
-let img;
-let img2;
-let img3;
-let img4;
-let img5;
-let img6;
-let img7;
-let img8;
-function preload() {
-  img = loadImage('data/shell.png');
-  img2 = loadImage('data/cat.png');
-  img3 = loadImage('data/orangecat.png');
-  img4 = loadImage('data/catsleep.png');
-  img5 = loadImage('data/realcat.png');
-  img6 = loadImage('data/home.png');
-  img7 = loadImage('data/camera.png');
-  img8 = loadImage('data/flash.png');
-}
 function draw() {
 
   noStroke();
@@ -97,7 +141,7 @@ function draw() {
   //image(img2, 160, 200, 180, 180);
   image(img6, 172, 205, 150, 147); //home
 
-  image(img5, 200, 287, 100, 70); // cat
+  image(img5, 230, 287, 80, 55); // cat
 
   image(img7, 398, 8, 60, 40); //camera
 
@@ -129,6 +173,7 @@ function draw() {
 
 
 
+
 //////***/////////***/////////***/////////***/////////***/////////***/////////***///
 
 //pop-up
@@ -138,6 +183,7 @@ function draw() {
     w: width / 8,
     h: height / 8,
   }
+
 
 
   //draw a rectangle
@@ -168,10 +214,39 @@ function draw() {
     textWrap(WORD);
     text('Tamagotchi was a portmanteau combining the two Japanese words tamago (たまご), which means "egg", and uotchi (ウオッチ) "watch". My game is an online version, and my virtual spin off is a game where you watch over a cat named maple for TaMAPLEgotchi. Tamagotchi was invented by Aki Maita and released by Bandai on November 23, 1996 in Japan and in the USA on May 1, 1997, quickly becoming one of the biggest toy fads of the late 1990s and the early 2000s.', 181, 195, width /3 - 13);
   }
+
+
   //////***/////////***/////////***/////////***/////////***/////////***/////////***///
 
+//action screen
+if (action){
+  image(img13, 172, 205, 150, 147); //action
 
+  image(img5, 230, 298, 80, 55); // cat
+}
 
+//home Fullscreen
+if (home){
+  image(img6, 172, 205, 150, 147); //home
+
+  image(img5, 230, 287, 80, 55); // cat
+}
+//shop Fullscreen
+if (shop){
+  image(img12,172, 205, 150, 147); //shop
+
+  image(img5, 170, 300, 80, 55); // cat
+}
+
+//oxff RGB (#color does not work in java or javascript)
+fill(255, 239, 85);
+ellipse(200, 404, 20, 20);
+ellipse(245, 417, 20, 20);
+ellipse(290, 406, 20, 20);
+
+image(img9, 237, 410, 15, 15); //house
+image(img10, 192, 398, 15, 15); //left
+image(img11, 285, 400, 15, 15); //right
 }
 
 // initially hide the divs using css or js
